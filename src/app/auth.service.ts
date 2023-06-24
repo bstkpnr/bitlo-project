04bb96f8cd1bf636c4e2ed5a3d15dc0526bbe4ea
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2023-06-24 20:52:24 +0300

    added profil service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,21 +1,38 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private BASE_URL='https://akademi-cp.bitlo.com/api/interview/auth';
+  private BASE_URL = 'https://akademi-cp.bitlo.com/api/interview/auth';
+  private token = 'HKKBITLO123TOKENABC';
 
-  constructor(private http:HttpClient) {}
-    login(username:string,password:string):Observable<any>{
-      const res={
-        identifier:username,
-        password:password
-      };
+  httpOptions() {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'x-bitlo-auth': this.token,
+      }),
+    };
+  }
+  constructor(private http: HttpClient) {}
 
-      return this.http.post(`${this.BASE_URL}/login`,res);
-    }
-   }
+  login(username: string, password: string): Observable<any> {
+    const res = {
+      identifier: username,
+      password: password,
+    };
 
+    return this.http.post(`${this.BASE_URL}/login`, res);
+  }
+  getUserProfil(): Observable<any> {
+    return this.http.post<any>(`${this.BASE_URL}/me`, {}, this.httpOptions());
+  }
+
+  isLoggedIn(): boolean {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+}
