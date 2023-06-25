2b59efc948dc9689331f61eb79d7e9e34b42518c
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2023-06-26 00:24:18 +0300

    edit fix

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  isLoggedIn: boolean = false; 
+export class HeaderComponent implements OnInit {
+  isLoggedIn: boolean = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.checkUserSession();
+  }
+
+  checkUserSession(): void {
+    const token = localStorage.getItem('token');
+    this.isLoggedIn = !!token;
+  }
+
+  logout(): void {
+    this.clearUserSession();
+
+    this.router.navigate(['/login']);
+  }
+
+  clearUserSession(): void {
+    localStorage.removeItem('token');
+    this.isLoggedIn = false;
+  }
 }
