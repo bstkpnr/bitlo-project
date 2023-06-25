afce7411d9c0a5e10338a032f49712b2152e4c4d
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2023-06-25 11:51:17 +0300

    create markets

---
+++ src/app/components/markets/markets.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { MarketService } from 'src/app/market.service';
+
+export interface Markets {
+  index: number;
+  marketCode: string;
+  currentQuote: string;
+  change24h: string;
+  change24hPercent: string;
+  highestQuote24h: string;
+  lowestQuote24h: string;
+  weightedAverage24h: string;
+  volume24h: string;
+}
+
+export type MarketsList = Markets[];
+
+@Component({
+  selector: 'markets',
+  templateUrl: './markets.component.html',
+  styleUrls: ['./markets.component.css']
+
+})
+export class MarketsComponent implements OnInit {
+  markets: Markets[] = [];
+  filterMarketData: Markets[] = [];
+
+  displayedColumns: string[] = [
+    'index',
+    'marketCode',
+    'currentQuote',
+    'change24h',
+    'change24hPercent',
+    'highestQuote24h',
+    'lowestQuote24h',
+  ];
+  constructor(private marketService: MarketService) {}
+
+  ngOnInit(): void {
+    this.getMarkets();
+
+  }
+  
+
+  filterData(filterVal: string) {
+    this.filterMarketData = this.markets.filter((market) =>
+      market.marketCode.toLowerCase().includes(filterVal.trim().toLowerCase())
+    );
+  }
+
+  getMarkets(): void {
+    this.marketService.getMarkets().subscribe((markets) => {
+      this.markets = markets;
+      this.filterMarketData = [...this.markets];
+    });
+  }
+
+  
+}
