b20ca1cd428b414d77d67a34773a43e9abfb9428
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2023-06-28 00:55:36 +0300

    edit fix

--- src/app/market.service.ts
+++ src/app/market.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Market {
+  marketCode: string;
+  currentQuote: number;
+  change24h: number;
+  change24hPercent: number;
+  highestQuote24h: number;
+  lowestQuote24h: number;
+  weightedAverage24h: number;
+  baseAsset: string;
+  baseAssetLogoUrl: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +26,8 @@ export class MarketService {
   getMarkets(): Observable<any> {
     return this.http.get<any>(this.BASE_URL);
   }
-  getMarketDetail(marketCode:string){
-    return this.http.get<any>(`${this.BASE_URL}/${marketCode}`);
+
+  getMarketsData(): Observable<Market[]> {
+    return this.http.get<Market[]>(this.BASE_URL);
   }
 }
